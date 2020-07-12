f9b4100cffb266ef77c8798b0cecc62511bd4b64
Leandro Martins <leosonic2@gmail.com>
2020-07-12 19:08:31 -0300

    correcao no endereco

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://backend-trabalho-mongo-8081.herokuapp.com/',
+  baseURL: 'https://backend-trabalho-mongo-8081.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
